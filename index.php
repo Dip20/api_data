14ad58291f927b6279e45cf68774784b4e81ce7c
dip sarkar <info.dip.sarkar@gmail.com>
2021-05-26 19:20:03 +0530

    bugs fixed, formatted and updated

--- index.php
+++ index.php
@@ -4,121 +4,122 @@ $conn = new mysqli ("localhost","root","","its-for-you");
 // Check connection
 if ($conn -> connect_errno)
 {
-  echo "Failed to connect to MySQL: " . $conn -> connect_error;
+ echo "Failed to connect to MySQL: " . $conn -> connect_error;
 
 }else{
-   // echo "connected";
+  // echo "connected";
 }
+// ............................................................................
 
-// init parameter..
-$output = "";
-$apikey = "";
-$apisecret = "";
-$query = "";
+ // init parameter..
+ $output = "";
+ $apikey = "";
+ $apisecret = "";
+ $query = "";
 
-// check for user parameter..
-if(isset($_GET['apikey']) AND isset($_GET['apisecret']) AND isset($_GET['query']))
-{
+ // check for user parameter..
+ if(isset($_GET['apikey']) AND isset($_GET['apisecret']) AND isset($_GET['query']))
+ {
+   $apikey = $_GET['apikey'];
+   $apisecret = $_GET['apisecret'];
+   $query = $_GET['query'];
 
-    $apikey = $_GET['apikey'];
-    $apisecret = $_GET['apisecret'];
-    $query = $_GET['query'];
-
-}else{
-  $obj = new stdClass();
-  $obj->status = 0;
-  $obj->error = "Please Enter all parameter. (Required parameter - apikey , apisecrest , query)";
-
-  $output = json_encode($obj);
-  echo $output;
-}
-
-
-    // if user apikey and all match our database.
+    // check for user apikey apisecret query should match with our database.
     $sql = mysqli_query($conn,"SELECT * FROM tbl_api_auth WHERE api_key='$apikey'");
     if($sql)
     {
-        if(mysqli_num_rows($sql)>0)
+      if(mysqli_num_rows($sql)>0)
         {
-            $data=mysqli_fetch_assoc($sql);
+          $data=mysqli_fetch_assoc($sql);
 
-            if ($data['api_key']==$apikey AND $data['api_secret']==$apisecret)
+          if ($apikey==$data['api_key'] AND $apisecret==$data['api_secret'])
+          {
+            // step 1 we will check requested query mean data table is valid or not.
+            $get_table = mysqli_query($conn,"SELECT * FROM tbl_master WHERE table_name='$query'");
+            if ($get_table)
             {
-                //    echo "api match";
-                // step 1 we will check requested query mean data table is valid or not.
+              if(mysqli_num_rows($get_table)>0)
+              {
+                $table_name = $query;
 
-
-                $get_table = mysqli_query($conn,"SELECT * FROM tbl_master WHERE table_name='$query'");
-                if ($get_table)
+                // table name is exist so go for next process..
+                // setp 2 get data from database and display to user.
+                 // query starts here
+                $get_data = mysqli_query($conn,"SELECT * FROM $table_name");
+                if ($get_data)
                 {
-                    if(mysqli_num_rows($get_table)>0)
+                  if(mysqli_num_rows($get_data)>0)
+                  {
+                    $var = array();
+                    while ($result=mysqli_fetch_assoc($get_data))
                     {
-                        $table_name = $query;
-
-                    // table name is exist so go for next process..
-
-                    // setp 2 get data from database and display to user.
-                     // query starts here
-                    $get_data = mysqli_query($conn,"SELECT * FROM $table_name");
-                    if ($get_data)
-                    {
-                        if(mysqli_num_rows($get_data)>0)
-                        {
-                            $var = array();
-                            while ($result=mysqli_fetch_assoc($get_data))
-                            {
-                                $var[] = $result;
-                            }
-
-                            $obj = new stdClass();
-                            $obj->status = 1;
-                            $obj->Data =  $var;
-
-                            $output = json_encode($obj);
-                            echo $output;
-
-                            // uncomment the bellow line if you wish to get data in array form..
-                            // $result = json_decode($output);
-                            // echo "<pre>";
-                            // print_r($result);
-                        }
+                        $var[] = $result;
                     }
-                // query ends here
+                    // output the json data
+                    $obj = new stdClass();
+                    $obj->status = 1;
+                    $obj->Data =  $var;
 
+                    $output = json_encode($obj);
+                    echo $output;
 
-                    }else
-                    {
-                      $obj = new stdClass();
-                      $obj->status = 0;
-                      $obj->error = "Invalid Query";
+                    // uncomment the bellow line if you wish to get data in array form..
+                    // $result = json_decode($output);
+                    // echo "<pre>";
+                    // print_r($result);
+                  }
+                }// query ends here
 
-                      $output = json_encode($obj);
-                      echo $output;
-                    }
-                }
+              }else
+              {
+                $obj = new stdClass();
+                $obj->status = 0;
+                $obj->error = "Invalid Query";
 
+                $output = json_encode($obj);
+                echo $output;
+              }
+           }
 
-            }else {
-              $obj = new stdClass();
-              $obj->status = 0;
-              $obj->error = "Invalid API key or API Secret";
+        }else
+        {
+          $obj = new stdClass();
+          $obj->status = 0;
+          $obj->error = "Invalid API key or API Secret";
 
-              $output = json_encode($obj);
-              echo $output;
-            }
-
+          $output = json_encode($obj);
+          echo $output;
         }
+
+      }else
+      {
+        $obj = new stdClass();
+        $obj->status = 0;
+        $obj->error = "Invalid API key or API Secret";
+
+        $output = json_encode($obj);
+        echo $output;
+      }
 
     }else
     {
       $obj = new stdClass();
       $obj->status = 0;
-      $obj->error = "Invalid API key or API Secret";
+      $obj->error = "SERVER NOT RESPONDING!";
 
       $output = json_encode($obj);
       echo $output;
     }
 
+  }else
+  {
+    $obj = new stdClass();
+    $obj->status = 0;
+    $obj->error = "Please Enter all parameter. (Required parameter - apikey , apisecrest , query)";
+
+    $output = json_encode($obj);
+    echo $output;
+  }
 
 
 
@@ -127,4 +128,19 @@ if(isset($_GET['apikey']) AND isset($_GET['apisecret']) AND isset($_GET['query']
 
 
 
-?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   ?>
